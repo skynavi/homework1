960b8305bc70e35038dcc6a069e98bfcd971ad67
Sergey Ilin <56452788+skynavi@users.noreply.github.com>
2024-06-27 21:00:09 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,6 @@
+exemple = 'Unknown'
+print(exemple[0]) # вывод первого символа
+print(exemple[-1]) # вывод последнего символа
+print(exemple[4:]) #  вторую половину этой строки, нечет кол-во
+print(exemple[::-1]) # слово наоборот
+print(exemple[1::2]) # каждый второй символ
